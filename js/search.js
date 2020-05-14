984691f4af2e361404a8cae54fc743445e45dcfe
Varsha Bhagi <varshabhagi@Varshas-MacBook-Pro.local>
2020-05-14 16:36:25 -0400

    textbox onclick change removed

--- js/search.js
+++ js/search.js
@@ -4,7 +4,12 @@ function activatePlacesSearch() {
     var input = document.getElementById('search_input');
     var autocomplete = new google.maps.places.Autocomplete(input);
 }
-function SelectAll(id) { 
-    document.getElementById(id).focus();
-    document.getElementById(id).select();
+
+function SelectAll() { 
+    document.getElementById('search_input').focus();
+    document.getElementById('search_input').select();
 } 
+
+function myFunction() {
+  document.getElementById('search_input').style.color = "red";
+}
