2bb8b0e8bf3e08ec2d7ccddbb07e26590519dc54
Varsha Bhagi <varshabhagi@Varshas-MacBook-Pro.local>
2020-05-13 22:57:10 -0400

    javascript search updated

--- js/search.js
+++ js/search.js
@@ -11,16 +11,5 @@ $(document).ready(function () {
         var near_place = autocomplete.getPlace();
         document.getElementById('loc_lat').value = near_place.geometry.location.lat();
         document.getElementById('loc_long').value = near_place.geometry.location.lng();
-		
-        document.getElementById('latitude_view').innerHTML = near_place.geometry.location.lat();
-        document.getElementById('longitude_view').innerHTML = near_place.geometry.location.lng();
     });
-});
-
-$(document).on('change', '#'+searchInput, function () {
-    document.getElementById('latitude_input').value = '';
-    document.getElementById('longitude_input').value = '';
-	
-    document.getElementById('latitude_view').innerHTML = '';
-    document.getElementById('longitude_view').innerHTML = '';
 });
