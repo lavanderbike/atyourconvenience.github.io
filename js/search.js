21e4bb63bd293c372bce42db25251fa8552bfb03
Varsha Bhagi <varshabhagi@Varshas-MacBook-Pro.local>
2020-05-13 20:55:48 -0400

    added search

--- js/search.js
+++ js/search.js
@@ -1,11 +1,26 @@
 /* JS Document */
-var map;
+var searchInput = 'search_input';
 
-function initialize(){
-
-	var mapOptions= {
-		zoom:8;
-		center: new google.maps.LatLng()
-	}
+$(document).ready(function () {
+    var autocomplete;
+    autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+        types: ['geocode'],
+    });
 	
-}
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var near_place = autocomplete.getPlace();
+        document.getElementById('loc_lat').value = near_place.geometry.location.lat();
+        document.getElementById('loc_long').value = near_place.geometry.location.lng();
+		
+        document.getElementById('latitude_view').innerHTML = near_place.geometry.location.lat();
+        document.getElementById('longitude_view').innerHTML = near_place.geometry.location.lng();
+    });
+});
+
+$(document).on('change', '#'+searchInput, function () {
+    document.getElementById('latitude_input').value = '';
+    document.getElementById('longitude_input').value = '';
+	
+    document.getElementById('latitude_view').innerHTML = '';
+    document.getElementById('longitude_view').innerHTML = '';
+});
